6bb36d78219444f8d4ecd9fe03e9ec5a963aaed1
Alexander Verkhoglyad <noreply>
2014-10-06 16:03:30 +0000

    Add module users

---
+++ users.js
@@ -0,0 +1,16 @@
+/**
+ * @module //users
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Users
+ * @extends Montage
+ */
+exports.Users = Montage.specialize(/** @lends Users# */ {
+    constructor: {
+        value: function Users() {
+            this.super();
+        }
+    }
+});
