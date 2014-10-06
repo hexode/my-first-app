06d4822bd90f8221facc6ed6835e9b4202f25313
Alexander Verkhoglyad <noreply>
2014-10-06 15:59:34 +0000

    Add component github-user

---
+++ ui/github-user.reel/github-user.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/github-user.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class GithubUser
+ * @extends Component
+ */
+exports.GithubUser = Component.specialize(/** @lends GithubUser# */ {
+    constructor: {
+        value: function GithubUser() {
+            this.super();
+        }
+    }
+});
